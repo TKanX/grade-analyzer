be76d36f847ec3dff983bb8bc650a7a5aeaf29af
Tony Kan <tianchengkan@gmail.com>
2024-12-26 14:18:42 -0800

    feat(project): Refactor server startup to use child processes for frontend and backend

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -37,7 +37,7 @@ const disconnect = async () => {
     return;
   }
 
-  console.log('Closing database connection');
+  console.log('Closing database connection...');
   await mongoose.disconnect();
 
   isConnected = false;
